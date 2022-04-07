7e98f82feacc5dd35ac36bec5e6b83868c933ba4
JasonHuang <lohas821019@gmail.com>
2022-04-07 11:49:29 +0800

    revise README.md

--- P_sobel.py
+++ P_sobel.py
@@ -16,23 +16,30 @@ while True:
     #Sobel
     x = cv2.Sobel(frame,cv2.CV_16S,1,0)
     y = cv2.Sobel(frame,cv2.CV_16S,0,1)
-
     absX = cv2.convertScaleAbs(x)
     absY = cv2.convertScaleAbs(y)
-
     dst = cv2.addWeighted(absX,0.5,absY,0.5,0.5)
     
-    #
+    #轉為灰階
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
+    #使用閥值
     _, mask = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    #使用Canny
+    thresh = cv2.Canny(gray, 128, 256)
+
+    #隨著使用方式修改要顯示的輪廓
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
     for cnt in contours:
         # Calculate area and remove small elements
         area = cv2.contourArea(cnt)
         if area > 10:
-            cv2.drawContours(mask, [cnt], -1, (0, 255, 0), 2)
+            cv2.drawContours(thresh, [cnt], -1, (0, 255, 0), 2)
 
+
+    cv2.imshow("thresh", thresh)
     cv2.imshow("mask", mask)
     cv2.imshow("frame", frame)
     cv2.imshow("dst", dst)
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Using Python  OpenCV to control the camera
 
 
 ![Figure_1.jpg](/resources/Figure_1.jpg)
+![Figure_2.jpg](/resources/Figure_2.jpg)
