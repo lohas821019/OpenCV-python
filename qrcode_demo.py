1fbc06af6e4fa64e86e13ede0a3491031134e86b
JasonHuang <lohas821019@gmail.com>
2022-09-01 13:48:57 +0800

    add qrcode test

--- qrcode_demo.py
+++ qrcode_demo.py
@@ -104,7 +104,83 @@ while True:
 cv2.destroyAllWindows()
 cap.release()
 
+#%%
 
+def detect_QRCode():
+    from pyzbar.pyzbar import decode
+    import cv2
+    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    cap.set(3,640)
+    cap.set(4,480)
+
+    while True:
+        _, frame = cap.read()
+        for barcode in decode(frame):
+            mydata = barcode.data.decode('utf-8')
+            print(mydata)
+            pts = np.array([barcode.polygon],np.int32)
+            cv2.polylines(frame,[pts],True,(255,0,255),5)
+            pts2 = barcode.rect
+            cv2.putText(frame, mydata, (pts2[0],pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,0.9, (255, 0, 255), 2)
+        cv2.imshow("frame", frame)
+        
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+#%%
+qrcode_flag = 0
+def detect_QRCode(qrcode_flag):
+    from pyzbar.pyzbar import decode
+    import cv2
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, frame = cap.read()
+        cv2.imshow("frame", frame)
+        k = cv2.waitKey(1) & 0xFF
+        
+        if k == 27:
+            break
+
+        for barcode in decode(frame):
+            mydata = barcode.data.decode('utf-8')
+            print(mydata)
+
+            pts = np.array([barcode.polygon],np.int32)
+            cv2.polylines(frame,[pts],True,(255,0,255),5)
+            pts2 = barcode.rect
+            cv2.putText(frame, mydata, (pts2[0],pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,0.9, (255, 0, 255), 2)
+
+            if len(mydata)>5:
+                qrcode_flag = 1
+
+        if qrcode_flag:
+            break
+
+
+    cv2.destroyAllWindows()
+    cap.release()
+    
+    #%%
+    
+while True:
+
+    rfid_flag = 1
+    print(1)
+    if rfid_flag:
+        rfid_flag = 0
+        break
+    
+
+qrcode_flag = 1
+print('qrcode_flag= '+ str(qrcode_flag))
+
+print(f'{qrcode_flag}=qrcode_flag123')
 
 
 
